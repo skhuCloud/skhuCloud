f1979451fd84a7db48fb0a65d9f764da8f1f0850
jaeyeon kim <jakind@naver.com>
2022-03-20 22:03:41 +0900

    #1 화면 기획 및 구현 완료

--- src/main/java/com/skhu/cloud/config/security/CustomUserDetailService.java
+++ src/main/java/com/skhu/cloud/config/security/CustomUserDetailService.java
@@ -1,21 +1,21 @@
-package com.skhu.cloud.config.security;
-
-import com.skhu.cloud.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@RequiredArgsConstructor
-@Service
-public class CustomUserDetailService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username) // 찾지 못하면 (사용자) exception 을 던짐
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-    }
-}
+//package com.skhu.cloud.config.security;
+//
+//import com.skhu.cloud.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class CustomUserDetailService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return userRepository.findByUsername(username) // 찾지 못하면 (사용자) exception 을 던짐
+//                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+//    }
+//}
--- src/main/java/com/skhu/cloud/service/UserService.java
+++ src/main/java/com/skhu/cloud/service/UserService.java
@@ -1,12 +1,17 @@
 package com.skhu.cloud.service;
 
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
+import com.skhu.cloud.entity.User;
 
-import java.util.Map;
+//import java.util.Map;
 
 public interface UserService {
 
-    Long signup(@RequestBody Map<String , String> user);
+//    Long signup(@RequestBody Map<String , String> user);
+//
+//    String login(@RequestBody Map<String , String> user);
 
-    String login(@RequestBody Map<String , String> user);
+    Long signup(User user , String password2);
+
+    User login(User user);
 }
