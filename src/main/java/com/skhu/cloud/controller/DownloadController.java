7f0527ef1971a432bfa063726fc7d180bce9df67
SinJeongEun <lisasje25@gmail.com>
2022-03-31 00:13:08 +0900

    #14 선택적 다운 구현 완료

--- src/main/java/com/skhu/cloud/controller/DownloadController.java
+++ src/main/java/com/skhu/cloud/controller/DownloadController.java
@@ -5,41 +5,40 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.zip.ZipOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/download")
+//@RequestMapping("/download")
 public class DownloadController {
 
     private final DownloadService downloadService;
 
-    // file download
-    // 해당 path 만 동적으로 다룰 수 있으면 될 듯
-    // 근데 이제 한번에 여러개의 path를 어떻게 넘기냐는 것이다.
-    @GetMapping("")
-    public void download(HttpServletResponse httpServletResponse) {
-        String path = "/Users/jaeyeonkim/testDirectory/test1.txt"; //download 할 로컬파일의 경로
-        downloadService.downloadOne(httpServletResponse,path);
+    @GetMapping("/download/checked")
+    public void downloadChecked(@RequestParam(value = "checkedFiles",required = false)List<String> checkedFiles, HttpServletResponse httpServletResponse) throws IOException {
+        System.out.println("실행 됨~~~~000~");
+
+        Queue<String> que_checked = new LinkedList<>();
+        for(String s : checkedFiles) {
+            que_checked.add(s);
+            System.out.println("~~~   " + s);
+        }
+
+        boolean toZip = downloadService.before(httpServletResponse,que_checked);
+
+        if(toZip){
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/zip");
+        }
+
     }
 
 
-
-    //.zip 형태로 다운로드
-    @GetMapping("zip")
-    public void downloadZip(HttpServletResponse httpServletResponse) throws IOException {
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        httpServletResponse.setContentType("application/zip");
-
-        String path = "/Users/jaeyeonkim/testDirectory"; //압축할 파일 위치
-
-        // 확실하게 이 zipName 에 문제가 있는 듯하다 , 즉 zipName의 문제는 정확히 말하면 아니고 , donwloadService.zipFile 의 문제이다.
-        downloadService.zipFile(httpServletResponse,path);
-    }
 }
--- src/main/java/com/skhu/cloud/service/DownloadService.java
+++ src/main/java/com/skhu/cloud/service/DownloadService.java
@@ -1,17 +1,132 @@
 package com.skhu.cloud.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public interface DownloadService {
+@Service
+@RequiredArgsConstructor
+public class DownloadService {
 
-    void downloadOne(HttpServletResponse httpServletResponse, String path);
+    //다운로드 시작 전 zip이 필요한 경우 return true
+    public boolean before(HttpServletResponse httpServletResponse,Queue<String> que_path) throws IOException {
+        if(que_path.size() == 1){
+            String path = que_path.peek();
+            File file = new File(path);
+            // que 사이즈 1 &&  파일인 경우
+            if(file.isFile()) {
+                que_path.clear();
+                downloadOne(httpServletResponse,file);
+                return false;
+            }else {
+                // que 사이즈 1 &&  폴더인 경우
+                file.delete();
+                zipFile(httpServletResponse,que_path);
+                return true;
+            }
+        }
+        //파일 2개 이상 -> zipFile()
+        zipFile(httpServletResponse,que_path);
 
-    void zipFile(HttpServletResponse httpServletResponse, String path) throws IOException;
+        return true;
+    }
 
-    void addFolder(ZipOutputStream zipOut, String relativePath) throws IOException;
 
-    void addFile(File subFile, ZipOutputStream zipOut, String relativePath) throws IOException;
+    //단일 파일 다운로드
+    public void downloadOne(HttpServletResponse httpServletResponse, File file){
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            OutputStream out = null;
+            out = httpServletResponse.getOutputStream();
+
+            //파일 작성하기
+            int read;
+            byte[] buffer = new byte[1024];
+
+            while ((read = fis.read(buffer)) >= 0){
+                out.write(buffer,0,read);
+            }
+
+            //attachment 로 설정함으로써 해당 데이터를 수신받은 브라우저가 팡링르 저장 또는 다른이름으로 저장 여부를 설정하게 할 수 있다.
+            httpServletResponse.setHeader("Content-Disposition", "attachment;filename=" + file.getName());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            //fis,out close 로직 작성하기
+        }
+    }
+
+
+    //다중 팡리 및 폴더 압축 진행
+    public void zipFile(HttpServletResponse httpServletResponse, Queue<String> que_path) throws IOException {
+        System.out.println("파일 압축 중 ,,,,,,,,,,,,,,");
+
+        httpServletResponse.setHeader("Content-Disposition","attachment; filename="+
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")) +".zip");
+
+        ZipOutputStream zipOut = new ZipOutputStream(httpServletResponse.getOutputStream());
+
+        while(!que_path.isEmpty()){
+            String path = que_path.poll();
+            File file = new File(path);
+            String rootAbsolutePath = file.getAbsolutePath();
+
+            if(file.isFile()) {
+                addFile(file,zipOut,file.getName());
+                continue;
+            }
+            Queue<File> queue = new LinkedList<>();
+            queue.add(file);
+
+            while (!queue.isEmpty()){
+                File folder = queue.poll();
+                for(File subFile : folder.listFiles()){
+                    String subfileAbsolutePath = subFile.getAbsolutePath();  ///Users/jeunning/myDownloadTest/subFolder
+                    String relativePath = subfileAbsolutePath.replace(rootAbsolutePath,file.getName());  //myDownloadTest/subFolder
+
+                    if(subFile.isDirectory()) {
+                        queue.add(subFile);
+                        addFolder(zipOut,relativePath);
+                    }
+                    else addFile(subFile, zipOut, relativePath);
+                }
+            }
+        }
+
+        zipOut.closeEntry();
+        zipOut.close();
+    }
+
+    public void addFolder(ZipOutputStream zipOut, String relativePath) throws IOException {
+        relativePath = relativePath.endsWith("/")?relativePath : relativePath + "/";
+        zipOut.putNextEntry(new ZipEntry(relativePath));
+    }
+
+    public void addFile(File subFile, ZipOutputStream zipOut, String relativePath) throws IOException {
+        FileInputStream fis = new FileInputStream(subFile);
+        zipOut.putNextEntry(new ZipEntry(relativePath));
+
+        byte[]buffer = new byte[1024];
+        int length;
+        while((length = fis.read(buffer)) >= 0){
+            zipOut.write(buffer,0,length);
+        }
+        fis.close();
+    }
+
+
 }
