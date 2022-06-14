1c37cc9d3cb1a3a0999df4e426cda38f9228a60c
SinJeongEun <lisasje25@gmail.com>
2022-06-15 00:10:57 +0900

    sort 완료

--- src/main/java/com/skhu/cloud/constant/Const.java
+++ src/main/java/com/skhu/cloud/constant/Const.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Const {
     public static final Long INIT_PAGE_NUMBER = 1L;
-    public static final Long PAGE_SIZE = 3L;
+    public static final Long PAGE_SIZE = 10L;
     public static final Long DEPTH_LIMIT = 8L; // Depth limit 은 8 이 적당하다, 그래야지 되게 로우 레벨한 곳에서 진행해도 된다.
     public static final String ACCESS_PROHIBIT_FILE = "Containers";
 }
